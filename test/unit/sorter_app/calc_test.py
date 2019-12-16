fbe26f377a63f94b4516294d3de20dc326bf0cc5
Peter K <pkjar16@student.aau.dk>
2019-12-16 23:26:07 +0100

    First Test

---
+++ test/unit/sorter_app/calc_test.py
@@ -0,0 +1,19 @@
+import unittest as ut
+import sys
+sys.path.append("sorter_app")
+from calc import Calculate
+
+class TestCalculate(ut.TestCase):
+    def setUp(self):
+        self.calc = Calculate()
+
+    def test_add_return_false(self):
+        self.assertEqual(4, self.calc.add(2,2))
+                   
+    def test_add_return_correct(self):
+        self.assertEqual(4, self.calc.add(2,2))
+
+
+if __name__ == '__main__':
+    ut.main()
+    
